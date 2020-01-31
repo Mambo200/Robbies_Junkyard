6d180ea811859129ca78297a32ccf095b1ab20b1
Tobias <tobis-stroedicke@gmx.de>
2020-01-31 23:48:32 +0100

    Add Project
    Click to move Player
    working inventory

---
+++ Game Jam 2020/Global Game Jam 2020/Assets/Scripts/Interact/Interactable.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class Interactable : MonoBehaviour
+{
+    public abstract Junk JunkType { get;  }
+    /// <summary>
+    /// Methods which will be executed at the end of <see cref="Click"/>
+    /// </summary>
+    [Tooltip("These Methods will be exectued at last!")]
+    public UnityEvent m_OnClicked;
+
+    /// <summary>
+    /// Methods which will be executed at the end of <see cref="Interact"/>
+    /// </summary>
+    [Tooltip("These Methods will be exectued at last!")]
+    public UnityEvent m_OnInteracted;
+
+
+    protected virtual void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// When object was clicked
+    /// </summary>
+    public virtual void Click()
+    {
+        m_OnClicked.Invoke();
+    }
+
+    /// <summary>
+    /// interact with object
+    /// </summary>
+    public virtual void Interact()
+    {
+        m_OnInteracted.Invoke();
+    }
+
+    /// <summary>
+    /// Add Item to inventory
+    /// </summary>
+    public void MoveToInventory(int _addCount)
+    {
+        FindObjectOfType<PlayerController>().inventory.PlayerInventory[JunkType] += _addCount;
+    }
+
+    /// <summary>
+    /// Destroy this Gameobject
+    /// </summary>
+    public void Destroy() => Destroy(this.gameObject);
+
+    public enum Junk
+    {
+        /// <summary>
+        /// Zahnrad
+        /// </summary>
+        COGWHEEL,
+
+        /// <summary>
+        /// Schraube
+        /// </summary>
+        BOLT,
+
+        /// <summary>
+        /// Schraubenmutter
+        /// </summary>
+        BOLTNUT,
+
+        /// <summary>
+        /// Lampe
+        /// </summary>
+        LAMP,
+    }
+}
---
+++ Game Jam 2020/Global Game Jam 2020/Assets/Scripts/Inventory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory
+{
+    public Dictionary<Interactable.Junk, int> PlayerInventory { get; private set; }
+
+    public Inventory()
+    {
+        PlayerInventory = new Dictionary<Interactable.Junk, int>();
+
+        for (int i = 0; i < System.Enum.GetValues(typeof(Interactable.Junk)).Length; i++)
+        {
+            PlayerInventory.Add((Interactable.Junk)i, 0);
+        }
+    }
+
+
+    public override string ToString()
+    {
+        if (PlayerInventory.Count == 0) return "No Items";
+
+        string toReturn = "Items: ";
+
+        foreach (KeyValuePair<Interactable.Junk, int> item in PlayerInventory)
+        {
+            toReturn += item.Key.ToString() + " " + item.Value.ToString() + " - ";
+        }
+
+        return toReturn.Remove(toReturn.Length - 2, 2);
+    }
+}
