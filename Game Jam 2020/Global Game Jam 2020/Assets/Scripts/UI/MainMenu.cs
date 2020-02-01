dfef866b3c81682d49f4c22bd6a47283ae6c0895
Bea <bea.trice@outlook.de>
2020-02-01 03:20:34 +0100

    Main Menu

---
+++ Game Jam 2020/Global Game Jam 2020/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("Game is starting");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+}
