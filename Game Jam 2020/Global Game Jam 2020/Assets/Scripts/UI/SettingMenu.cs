4561ba81fc6ade654cfb4900c97905c85db2544e
Bea <bea.trice@outlook.de>
2020-02-01 09:27:43 +0100

    More menus

---
+++ Game Jam 2020/Global Game Jam 2020/Assets/Scripts/UI/SettingMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingMenu : MonoBehaviour
+{
+    public void QuitButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
