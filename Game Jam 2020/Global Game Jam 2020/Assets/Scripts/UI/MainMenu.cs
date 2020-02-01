aa3c09d6a4f738f65cce471a6eac980657870858
Tobias <tobis-stroedicke@gmx.de>
2020-02-01 19:40:43 +0100

    Add old Assets, save Name
    Save name at beginning.
    recreate prefabs

--- Game Jam 2020/Global Game Jam 2020/Assets/Scripts/UI/MainMenu.cs
+++ Game Jam 2020/Global Game Jam 2020/Assets/Scripts/UI/MainMenu.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public void StartButton()
+    public void StartButton(InputField _inputField)
     {
+        if(_inputField.text == null
+            || string.IsNullOrWhiteSpace(_inputField.text)
+            || string.IsNullOrEmpty(_inputField.text))
+        {
+            Debug.Log("Null or white space or empty");
+            return;
+        }
+        PlayerController.PlayerName = _inputField.text;
         SceneManager.LoadScene(1);
         Debug.Log("Game is starting");
     }
