0cffc5c13df85152ae54617aaaa24b11f46c866b
Bea <bea.trice@outlook.de>
2020-02-02 17:42:11 +0100

    Revert "Merge branch 'RobbieSays'"
    This reverts commit d8b16dbd5c733246cd4386903cacd8b4f0ac6c48, reversing
    changes made to a1c0728b294f971e4858cec2badf98c092086222.

---
+++ Game Jam 2020/Global Game Jam 2020/Assets/Scripts/Characters/ReadRobbieText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReadRobbieText : MonoBehaviour
+{
+    public Text ChangeText;
+    public TextAsset Raw { get; private set; }
+
+    public string[] Lines { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadFile();
+    }
+
+    public void LoadFile()
+    {
+        Raw = Resources.Load<TextAsset>("Text/Robbie_Text");
+
+#if(DEBUG)
+        if (Raw == null)
+        {
+            Debug.LogError("Robbie Text could not be loaded!", this);
+            Debug.Break();
+        }
+#endif
+
+        Lines = Raw.text.Split('\n');
+    }
+
+    public void GetRandomText()
+    {
+        ChangeText.text = Lines[MyRandom.GetRandomNumber(Lines.Length)];
+    }
+
+}
---
+++ Game Jam 2020/Global Game Jam 2020/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    private static PauseManager instance;
+    public static PauseManager Get
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindObjectOfType<PauseManager>();
+            }
+            return instance;
+        }
+    }
+
+    public bool GamePaused { get => !m_PauseButton.activeSelf; }
+
+    [Header("Root Gameobjects")]
+    [SerializeField] private GameObject m_ChangeRoomsRoot;
+    [SerializeField] private GameObject m_PauseButton;
+    [SerializeField] private GameObject m_SettingsMenu;
+    [SerializeField] private GameObject m_InventoryUI;
+    [SerializeField] private GameObject m_BuildTool;
+
+    [Header("Text")]
+    [SerializeField] private UnityEngine.UI.Text m_Ducktape;
+    [SerializeField] private UnityEngine.UI.Text m_Flasche;
+    [SerializeField] private UnityEngine.UI.Text m_Holzbrett;
+    [SerializeField] private UnityEngine.UI.Text m_Schraubenmutter;
+    [SerializeField] private UnityEngine.UI.Text m_Pappe;
+    [SerializeField] private UnityEngine.UI.Text m_Schraube;
+    [SerializeField] private UnityEngine.UI.Text m_Startknopf;
+    [SerializeField] private UnityEngine.UI.Text m_Wellblech;
+    [SerializeField] private UnityEngine.UI.Text m_Zahnrad;
+    [SerializeField] private UnityEngine.UI.Text m_Gas;
+    [SerializeField] private UnityEngine.UI.Text m_Zange;
+    [SerializeField] private UnityEngine.UI.Text m_Hammer;
+
+    [Header("Infotext")]
+    [SerializeField] private UnityEngine.UI.Text m_InfoText;
+
+
+    private Inventory playerInventory;
+
+    public void SetInventory(Inventory _inventory)
+    {
+        playerInventory = _inventory;
+    }
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    public void PauseGame()
+    {
+        m_ChangeRoomsRoot.SetActive(false);
+        m_PauseButton.SetActive(false);
+
+        m_InventoryUI.SetActive(true);
+        FillInventory();
+        m_SettingsMenu.SetActive(true);
+        m_BuildTool.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        m_ChangeRoomsRoot.SetActive(true);
+        m_PauseButton.SetActive(true);
+
+        m_InventoryUI.SetActive(false);
+        m_SettingsMenu.SetActive(false);
+        m_BuildTool.SetActive(false);
+    }
+
+    /// <summary>
+    /// Refresh inventory text
+    /// </summary>
+    public void FillInventory()
+    {
+        if (playerInventory == null) GetInventory();
+
+        m_Ducktape.text = playerInventory.JunkInventory[Interactable.Junk.DUCKTAPE].ToString();
+        m_Flasche.text = playerInventory.JunkInventory[Interactable.Junk.BOTTLE].ToString();
+        m_Holzbrett.text = playerInventory.JunkInventory[Interactable.Junk.WOODENPLANK].ToString();
+        m_Schraubenmutter.text = playerInventory.JunkInventory[Interactable.Junk.BOLTNUT].ToString();
+        m_Pappe.text = playerInventory.JunkInventory[Interactable.Junk.CARDBOARD].ToString();
+        m_Schraube.text = playerInventory.JunkInventory[Interactable.Junk.BOLT].ToString();
+        m_Startknopf.text = playerInventory.JunkInventory[Interactable.Junk.STARTBUTTON].ToString();
+        m_Wellblech.text = playerInventory.JunkInventory[Interactable.Junk.CORRUGATEDIRON].ToString();
+        m_Zahnrad.text = playerInventory.JunkInventory[Interactable.Junk.COGWHEEL].ToString();
+
+        m_Zange.text = playerInventory.ToolInventory[Interactable.Tools.TONGS].ToString();
+        m_Hammer.text = playerInventory.ToolInventory[Interactable.Tools.HAMMER].ToString();
+
+        m_Gas.text = playerInventory.FuelInventory[Interactable.Fuel.GAS].ToString();
+    }
+
+    private void GetInventory() => playerInventory = FindObjectOfType<PlayerController>().inventory;
+}
